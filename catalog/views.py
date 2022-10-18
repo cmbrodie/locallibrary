1b048a6e571a0d37280c4d30b8abcff05f27e515
cmbrodie <cmbrodiegt@gmail.com>
2022-10-18 15:29:55 -0400

    Finished part 6 of the MDN Tutorial, built books and authors list/detail views, and corresponding templates.

--- catalog/views.py
+++ catalog/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 
 
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 # Create your views here.
 def index(request):
     """View function for home page of site"""
 
     # Generate counts of some of the main objects
+    booklist = Book.objects.all()
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
     bookword = Book.objects.filter(title__contains="lord").count()
@@ -26,6 +28,36 @@ def index(request):
         "num_instances": num_instances,
         "num_instances_available": num_instances_available,
         "num_authors": num_authors,
+        "booklist": booklist,
     }
 
     return render(request, "index.html", context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    # context_object_name = "book_list"
+    # queryset = Book.objects.filter(title__contains="Si")
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context dict
+        return context
+
+    # change the ordering:
+    queryset = Book.objects.order_by("title")
+    paginate_by = 2
+    # pages accessed by passing GET params: to access page 2 /catalog/books/?page=2
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    # context_object_name = "author_list"
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
